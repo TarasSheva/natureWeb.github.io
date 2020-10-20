550e3e468814bb40bba2528653090cb2ac9e4225
TarasSheva <tarasgsheva@gmail.com>
2020-10-20 19:32:29 +0300

    modif

--- js/main.js
+++ js/main.js
@@ -34,3 +34,88 @@ if (heroBtn !== null) {
     btn1.addEventListener('click', toVideo);
     btn2.addEventListener('click', toCollection)
 }
+
+let windowSlider = document.querySelector(".card_slider");
+
+let slider = document.querySelectorAll(".card_item");
+console.log(slider);
+console.log(windowSlider);
+
+[].forEach.call(slider, function (element) {
+    element.onclick = function () {
+        windowSlider.style.display = "block";
+
+        let col = this.getAttribute("id");
+
+
+        let slideIndex = Number(col);
+        showSlides(slideIndex);
+
+        function showSlides(slideIndex) {
+            let slides = document.getElementsByClassName("slides");
+
+            let prev = document.querySelector('.prev');
+            prev.onclick = function () {
+                slideIndex--;
+                if (slideIndex < 1) {
+                    slideIndex = slides.length;
+                }
+                showSlides(slideIndex);
+
+            };
+            let next = document.querySelector('.next');
+            next.onclick = function () {
+                slideIndex++;
+                if (slideIndex > slides.length) {
+                    slideIndex = 1;
+                }
+                showSlides(slideIndex);
+            };
+
+            for (let i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            slides[slideIndex - 1].style.display = "block";
+        }
+    }
+})
+
+
+
+// [].forEach.call(slider, function (el) {
+//     el.onclick = function () {
+//         windowSlider.style.display = 'block';
+//         let col = this.getAttribute("id");
+//
+//         let slideIndex = Number(col.slice(-1));
+//         showSlides(slideIndex);
+//
+//         function showSlides(slideIndex) {
+//             let slides = document.getElementsByClassName("my_slides");
+//
+//             let prev = document.querySelector('.prev');
+//             prev.onclick = function () {
+//                 slideIndex--;
+//                 if (slideIndex < 1) {
+//                     slideIndex = slides.length;
+//                 }
+//                 showSlides(slideIndex);
+//
+//             };
+//             let next = document.querySelector('.next');
+//             next.onclick = function () {
+//                 slideIndex++;
+//                 if (slideIndex > slides.length) {
+//                     slideIndex = 1;
+//                 }
+//                 showSlides(slideIndex);
+//             };
+//
+//             for (let i = 0; i < slides.length; i++) {
+//                 slides[i].style.display = "none";
+//             }
+//             slides[slideIndex - 1].style.display = "block";
+//         }
+//     };
+//
+// });
